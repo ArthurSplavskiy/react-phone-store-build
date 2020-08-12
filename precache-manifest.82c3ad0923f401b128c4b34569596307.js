f8d07ba0f1cd06d58057c841cf6aa8e0589ca15e
Arthur <gitmail10@gmail.com>
2020-08-12 23:09:10 +0300

    first commit

---
+++ precache-manifest.82c3ad0923f401b128c4b34569596307.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73519ad59b91e19a052a1312f060d9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887ffd45ec40e04c1183",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eb60d071358a698b2f8f",
+    "url": "/static/css/main.27fbaf52.chunk.css"
+  },
+  {
+    "revision": "887ffd45ec40e04c1183",
+    "url": "/static/js/2.d85862e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d85862e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb60d071358a698b2f8f",
+    "url": "/static/js/main.2273e395.chunk.js"
+  },
+  {
+    "revision": "7f4775f6f5a85bb21ad0",
+    "url": "/static/js/runtime-main.cdacec15.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
